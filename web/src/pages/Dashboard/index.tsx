52414ea9fe4434b2020f8efa04d029571e9db3be
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-11-15 15:46:10 -0300

    feat(dashboard): adding transaction map at Dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -41,6 +41,9 @@ const Dashboard: React.FC = () => {
         (transaction: Transaction) => ({
           ...transaction,
           formattedValue: formatValue(transaction.value),
+          formattedDate: new Date(transaction.created_at).toLocaleDateString(
+            'pt-br',
+          ),
         }),
       );
 
@@ -97,16 +100,17 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                {transactions.map(transaction => (
-                  <tr>
-                    <td className="title">Website Hosting</td>
-                    <td className="outcome">- R$ 1.000,00</td>
-                    <td>Hosting</td>
-                    <td>19/04/2020</td>
-                  </tr>
-                ))}
-              </tr>
+              {transactions.map(transaction => (
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && ' - '}
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
